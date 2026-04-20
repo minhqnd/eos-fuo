3acdf5335d04b6ed2e9972dda003abb7c933c99d
Quang Minh <minhqnd@gmail.com>
2026-04-21 01:00:45 +0700

    feat: add DesktopOnlyGuard component to restrict access on non-desktop devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import DesktopOnlyGuard from "./desktop-only-guard";
 import "./base.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       lang="vi"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <DesktopOnlyGuard>{children}</DesktopOnlyGuard>
+      </body>
     </html>
   );
 }
