d8056eefe0a83e542ce1a5d4e06d01456ab2e868
Quang Minh <minhqnd@gmail.com>
2026-04-21 07:45:24 +0700

    Refactor DisableReactDevToolsInProduction to use disable-devtool package for improved functionality and cleaner code

--- src/app/disable-react-devtools.tsx
+++ src/app/disable-react-devtools.tsx
@@ -1,46 +1,15 @@
 "use client";
 
 import { useEffect } from "react";
-
-type ReactDevToolsHook = {
-    isDisabled?: boolean;
-    inject?: (...args: unknown[]) => void;
-    onCommitFiberRoot?: (...args: unknown[]) => void;
-    onCommitFiberUnmount?: (...args: unknown[]) => void;
-    [key: string]: unknown;
-};
-
-declare global {
-    interface Window {
-        __REACT_DEVTOOLS_GLOBAL_HOOK__?: ReactDevToolsHook;
-    }
-}
-
-const noop = () => {};
+import DisableDevtool from "disable-devtool";
 
 export default function DisableReactDevToolsInProduction() {
     useEffect(() => {
         if (process.env.NODE_ENV !== "production") return;
 
-        const existingHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-        if (existingHook) {
-            existingHook.isDisabled = true;
-            existingHook.inject = noop;
-            existingHook.onCommitFiberRoot = noop;
-            existingHook.onCommitFiberUnmount = noop;
-        }
-
-        Object.defineProperty(window, "__REACT_DEVTOOLS_GLOBAL_HOOK__", {
-            configurable: false,
-            enumerable: false,
-            writable: false,
-            value: {
-                isDisabled: true,
-                inject: noop,
-                onCommitFiberRoot: noop,
-                onCommitFiberUnmount: noop,
-                supportsFiber: true,
-            } satisfies ReactDevToolsHook,
+        DisableDevtool({
+            disableMenu: true,
+            clearLog: true,
         });
     }, []);
 
